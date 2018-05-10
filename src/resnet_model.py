1041f2e97689531cd8b767586c4a38f89de9ba0c
Luke Arend <larend@mit.edu>
2018-05-10 18:03:35 -0400

    debug

--- src/resnet_model.py
+++ src/resnet_model.py
@@ -48,10 +48,11 @@ def batch_norm(inputs, training, data_format):
   """Performs a batch normalization using a standard set of parameters."""
   # We set fused=True for a significant performance boost. See
   # https://www.tensorflow.org/performance/performance_guide#common_fused_ops
-  return tf.layers.batch_normalization(
-      inputs=inputs, axis=1 if data_format == 'channels_first' else 3,
-      momentum=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, center=True,
-      scale=True, training=training, fused=True)
+  return tf.contrib.layers.batch_norm(
+      inputs=inputs,
+      data_format='NCHW' if data_format == 'channels_first' else 'NHWC',
+      decay=_BATCH_NORM_DECAY, epsilon=_BATCH_NORM_EPSILON, center=True,
+      scale=True, is_training=training, fused=True)
 
 
 def fixed_padding(inputs, kernel_size, data_format):
