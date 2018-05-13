ceb86197a3bf95c95df1005a3e8b7b73e83c107a
Luke Arend <larend@mit.edu>
2018-05-13 18:01:53 -0400

    fixed import script too

--- exps/evaluate/evaluate-with-imports.py
+++ exps/evaluate/evaluate-with-imports.py
@@ -5,7 +5,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--model_dir', type=str, required=True)
 parser.add_argument('--scale_factor', type=float, required=True)
-parser.add_argument('--use_batch_norm', type=bool, required=True)
+parser.add_argument('--disable_batch_norm', dest='use_batch_norm',
+                    action='store_false')
 parser.add_argument('--dataset', type-str, required=True)
 FLAGS = parser.parse_args()
 
