a06a38f66b1c7b940d3d9ff676044520286eb6b5
Luke Arend <larend@mit.edu>
2018-05-16 12:05:13 -0400

    unit test activations

--- src/estimator.py
+++ src/estimator.py
@@ -249,6 +249,10 @@ class Estimator(object):
         predictions = model.predict(input_fn,
                                     predict_keys=['classes', 'activations'])
 
+        with tf.Session() as sess:
+            res = sess.run(predictions)
+        print(res)
+
         print('predict step completed')
         print(predictions)
 
