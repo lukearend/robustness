09f532fa4b70c8e9142c7bb6bba009236292779c
Luke Arend <larend@mit.edu>
2018-05-17 10:43:01 -0400

    unit test

--- exps/analysis/unit_test_activations.py
+++ exps/analysis/unit_test_activations.py
@@ -49,7 +49,7 @@ def main():
                 'num_filters': num_filters},
             tf_random_seed=int(time.time()))
 
-        for split in ['validation']:
+        for split in ['validation', 'test']:
             print('split: {}'.format(split))
 
             t_0 = time.time()
