fb9cb7543e4dd768d25a09ebb97cbe88f570eb57
Luke Arend <larend@mit.edu>
2018-05-18 00:01:54 -0400

    job array

---
+++ exps/analysis/slurm/imagenet.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --time=18:00:00
+#SBATCH --mem=64000
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:tesla-k80:1
+#SBATCH --job-name=im%a
+#SBATCH --output=out/imagenet-0000%a.out
+#SBATCH --mail-user=larend@mit.edu
+#SBATCH --mail-type=FAIL
+
+SCALE_FACTOR=('0.25' '0.5' '1' '2' '4')
+
+module load openmind/singularity/older_versions/2.4
+
+singularity exec --nv -B /om:/om /om/user/larend/localtensorflow.img \
+python /om/user/larend/robust/exps/analysis/activations.py \
+--model_dir=/om/user/larend/models/robust/imagenet/0000${SLURM_ARRAY_TASK_ID} \
+--scale_factor=${SCALE_FACTOR[$SLURM_ARRAY_TASK_ID]} \
+--dataset=imagenet \
+--pickle_dir=/om/user/larend/pickles2/imagenet/0000${SLURM_ARRAY_TASK_ID} \
+--host_filesystem=/om
+
+singularity exec --nv -B /om:/om /om/user/larend/localtensorflow.img \
+python /om/user/larend/robust/exps/analysis/redundancy.py \
+--pickle_dir=/om/user/larend/pickles2/imagenet/0000${SLURM_ARRAY_TASK_ID}
+
+singularity exec --nv -B /om:/om /om/user/larend/localtensorflow.img \
+python /om/user/larend/robust/exps/analysis/robustness.py \
+--model_dir=/om/user/larend/models/robust/imagenet/0000${SLURM_ARRAY_TASK_ID} \
+--scale_factor=${SCALE_FACTOR[$SLURM_ARRAY_TASK_ID]} \
+--dataset=imagenet \
+--pickle_dir=/om/user/larend/pickles2/imagenet/0000${SLURM_ARRAY_TASK_ID} \
+--host_filesystem=/om
