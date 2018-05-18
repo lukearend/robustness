2f1d446f1e5f25504ef66a33f9b7bfdeabf8f07d
Luke Arend <larend@mit.edu>
2018-05-18 00:41:03 -0400

    dgx

---
+++ exps/analysis/imagenet-dgx.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --mem=50000
+#SBATCH --cpus-per-task=80
+#SBATCH --job-name=imagenet
+#SBATCH --output=out/imagenet-0000%a.out
+#SBATCH --mail-user=larend@mit.edu
+#SBATCH --mail-type=FAIL
+
+SCALE_FACTOR=('0.25' '0.5' '1' '2' '4')
+
+module load openmind/singularity/older_versions/2.4
+
+singularity exec --nv -B /raid:/raid /raid/poggio/home/larend/localtensorflow.img \
+python /raid/poggio/home/larend/robust/exps/analysis/activations.py \
+--model_dir=/raid/poggio/home/larend/models/robust/imagenet/0000${SLURM_ARRAY_TASK_ID} \
+--scale_factor=${SCALE_FACTOR[$SLURM_ARRAY_TASK_ID]} \
+--dataset=imagenet \
+--pickle_dir=/raid/poggio/home/larend/pickles2/imagenet/0000${SLURM_ARRAY_TASK_ID} \
+--host_filesystem=/raid
+
+singularity exec --nv -B /raid:/raid /raid/poggio/home/larend/localtensorflow.img \
+python /raid/poggio/home/larend/robust/exps/analysis/redundancy.py \
+--pickle_dir=/raid/poggio/home/larend/pickles2/imagenet/0000${SLURM_ARRAY_TASK_ID}
+
+singularity exec --nv -B /raid:/raid /raid/poggio/home/larend/localtensorflow.img \
+python /raid/poggio/home/larend/robust/exps/analysis/robustness.py \
+--model_dir=/raid/poggio/home/larend/models/robust/imagenet/0000${SLURM_ARRAY_TASK_ID} \
+--scale_factor=${SCALE_FACTOR[$SLURM_ARRAY_TASK_ID]} \
+--dataset=imagenet \
+--pickle_dir=/raid/poggio/home/larend/pickles2/imagenet/0000${SLURM_ARRAY_TASK_ID} \
+--host_filesystem=/raid
