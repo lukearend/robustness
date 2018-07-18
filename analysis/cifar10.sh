808a2c8b36f4a2fff83a156363027ab3d9f21bb8
Luke Arend <larend@mit.edu>
2018-07-18 11:06:50 -0400

    debug

--- analysis/cifar10.sh
+++ analysis/cifar10.sh
@@ -14,7 +14,7 @@ BATCH_NORM_FLAG=('' '' '' '' '' '--disable_batch_norm' '--disable_batch_norm' '-
 module load openmind/singularity/older_versions/2.4
 
 singularity exec --nv -B /om:/om /om/user/larend/localtensorflow.img \
-python /om/user/larend/robust/exps/analysis/activations.py \
+python /om/user/larend/robust/analysis/activations.py \
 --model_dir=/om/user/larend/models/robust/cifar10/0000${SLURM_ARRAY_TASK_ID} \
 ${BATCH_NORM_FLAG[$SLURM_ARRAY_TASK_ID]} \
 --scale_factor=${SCALE_FACTOR[$SLURM_ARRAY_TASK_ID]} \
@@ -23,11 +23,11 @@ ${BATCH_NORM_FLAG[$SLURM_ARRAY_TASK_ID]} \
 --host_filesystem=/om
 
 singularity exec --nv -B /om:/om /om/user/larend/localtensorflow.img \
-python /om/user/larend/robust/exps/analysis/redundancy.py \
+python /om/user/larend/robust/analysis/redundancy.py \
 --pickle_dir=/om/user/larend/robustpickles/cifar10/0000${SLURM_ARRAY_TASK_ID}
 
 singularity exec --nv -B /om:/om /om/user/larend/localtensorflow.img \
-python /om/user/larend/robust/exps/analysis/robustness.py \
+python /om/user/larend/robust/analysis/robustness.py \
 --model_dir=/om/user/larend/models/robust/cifar10/0000${SLURM_ARRAY_TASK_ID} \
 ${BATCH_NORM_FLAG[$SLURM_ARRAY_TASK_ID]} \
 --scale_factor=${SCALE_FACTOR[$SLURM_ARRAY_TASK_ID]} \
--- analysis/imagenet.sh
+++ analysis/imagenet.sh
@@ -13,7 +13,7 @@ SCALE_FACTOR=('0.25' '0.5' '1' '2' '4')
 module load openmind/singularity/older_versions/2.4
 
 singularity exec --nv -B /om:/om /om/user/larend/localtensorflow.img \
-python /om/user/larend/robust/exps/analysis/activations.py \
+python /om/user/larend/robust/analysis/activations.py \
 --model_dir=/om/user/larend/models/robust/imagenet/0000${SLURM_ARRAY_TASK_ID} \
 --scale_factor=${SCALE_FACTOR[$SLURM_ARRAY_TASK_ID]} \
 --dataset=imagenet \
@@ -21,11 +21,11 @@ python /om/user/larend/robust/exps/analysis/activations.py \
 --host_filesystem=/om
 
 singularity exec --nv -B /om:/om /om/user/larend/localtensorflow.img \
-python /om/user/larend/robust/exps/analysis/redundancy.py \
+python /om/user/larend/robust/analysis/redundancy.py \
 --pickle_dir=/om/user/larend/robustpickles/imagenet/0000${SLURM_ARRAY_TASK_ID}
 
 singularity exec --nv -B /om:/om /om/user/larend/localtensorflow.img \
-python /om/user/larend/robust/exps/analysis/robustness.py \
+python /om/user/larend/robust/analysis/robustness.py \
 --model_dir=/om/user/larend/models/robust/imagenet/0000${SLURM_ARRAY_TASK_ID} \
 --scale_factor=${SCALE_FACTOR[$SLURM_ARRAY_TASK_ID]} \
 --dataset=imagenet \
